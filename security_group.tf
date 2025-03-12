1177d45420846ca9d235bf30b8585ddc3828999b
Hlib Kalinichev <hlib.kalinichev@ukma.edu.ua>
2025-03-12 23:54:19 +0200

    security_group rules fix.

--- security_group.tf
+++ security_group.tf
@@ -8,29 +8,27 @@ resource "aws_security_group" "terraform_sec_gr" {
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_vps_security_group_ingress_rule" "allow_all_ip_traffic_p22" {
+resource "aws_vpc_security_group_ingress_rule" "allow_all_ip_traffic_p22" {
   security_group_id = aws_security_group.terraform_sec_gr.id
   from_port = 22
   to_port = 22
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  ipv6_cidr_blocks = ["::/0"]
+  ip_protocol = "tcp"
+  cidr_ipv4 = "0.0.0.0/0"
 }
 
-resource "aws_vps_security_group_ingress_rule" "allow_self_ip_traffic_p5432_35" {
+resource "aws_vpc_security_group_ingress_rule" "allow_self_ip_traffic_p5432_35" {
   security_group_id = aws_security_group.terraform_sec_gr.id
   from_port = 5432
   to_port = 5435
-  protocol  = "tcp"
-  self = true
+  ip_protocol = "tcp"
+  referenced_security_group_id = aws_security_group.terraform_sec_gr.id
 }
 
-resource "aws_vps_security_group_egress_rule" "allow_all_outbound_traffic" {
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound_traffic" {
   security_group_id = aws_security_group.terraform_sec_gr.id
   from_port = 0
   to_port = 0
-  protocol = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-  ipv6_cidr_blocks = ["::/0"]
+  ip_protocol = "-1"
+  cidr_ipv4 = "0.0.0.0/0"
 }
 
